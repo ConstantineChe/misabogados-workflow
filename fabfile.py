c8eef34975a5bfebef0a946f6ea16b786b41b0a4
Konstantin Cherkashin <che.constantine@gmail.com>
2016-03-11 13:49:16 -0300

    improve deployments script

--- fabfile.py
+++ fabfile.py
@@ -10,20 +10,58 @@ env.hosts = hosts
 env.user = user
 env.forward_agent = True
 
-workflow_repo =  "git@bitbucket.org:constantineche/misabogados-workflow.git"
-
+project = "misabogados_workflow"
+repo =  "git@bitbucket.org:constantineche/misabogados-workflow.git"
 deploy_location = "/var/deploy/"
+
+class Instance:
+
+    def __init__(self):
+        self.echo = "default"
+        self.opts = {'DATABASE_URL': 'mongodb://127.0.0.1/%s_dev' % project,
+                     'PORT': '3000',
+                     'LOG_PATH': '/var/deploy/log/%s.log' % project}
+
+    def production(self):
+        self.echo = "production"
+        self.opts['DATABASE_URL'] = 'mongodb://127.0.0.1/%s_production' % project
+        self.opts['PORT'] = '8080'
+        self.opts['LOG_PATH'] = '/var/deploy/log/%s_production.log' % project
+
+    def staging(self):
+        self.echo = "staging"
+        self.opts['DATABASE_URL'] = 'mongodb://127.0.0.1/%s_staging' % project
+        self.opts['PORT'] = '3000'
+        self.opts['LOG_PATH'] = '/var/deploy/log/%s_staging.log' % project
+        self.opts['TEST'] = 'test'
+
+    def get_opts_string(self):
+        return ' '.join(map(lambda (k, v): k+'='+v, self.opts.iteritems()))
+
+
+instance = Instance()
+
+
+#============ Tasks =============
+
+def production():
+    instance.production()
+
+def staging():
+    instance.staging()
+
 
 def deploy(branch="master"):
     local("ssh-agent")
     local("ssh-add")
     with cd(deploy_location):
-        if not files.exists("misabogados-workflow"):
-            run("git clone " + workflow_repo)
-            run("git checkout " + branch)
-            run("ln -s . " + deploy_location + "current")
-            run("lein uberjar")
-    with cd(deploy_location + "misabogados-workflow"):
+        if not files.exists(project):
+            run("git clone " + repo)
+            with cd(deploy_location + project):
+                run("git checkout " + branch)
+                run("ln -s . " + deploy_location + "current")
+                run("lein uberjar")
+    with cd(deploy_location + project):
         run("git checkout " + branch)
         run("git pull")
         run("lein clean")
@@ -31,17 +69,26 @@ def deploy(branch="master"):
         runapp()
 
 def check():
-    run("ps -ef | grep misabogados-workflow.jar")
+    run("ps -ef | grep %s.jar" % project)
 
 
 def runapp():
-    with cd(deploy_location + "misabogados-workflow"):
+    with cd(deploy_location + project):
         if files.exists("pid"):
             pid = run("cat pid")
             with settings(warn_only=True):
                 run("kill -9 " + pid.split(" ")[-1])
             run("rm pid")
-        run("sh -c '((nohup %s > /dev/null 2> /dev/null) & echo $! > pid)'" % "java -jar target/misabogados-workflow.jar", pty=False)
+        # Very powerful black magic
+        run("sh -c '((%s nohup %s > /dev/null 2> /dev/null) & echo $! > pid)'" % (instance.get_opts_string(), "java -jar target/uberjar/%s.jar" % project), pty=False)
 
 def kill(pid):
     sudo("kill -9 " + pid)
+
+def log(file=project+".log"):
+    run("ls /var/deploy/log")
+    run("tail -n 100 /var/deploy/log/" + file)
+
+def test():
+    run("echo " + instance.echo)
+    run(instance.get_opts_string() + " sh -c 'echo $LOG_PATH $TEST'")
