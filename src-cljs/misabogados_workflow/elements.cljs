3f1e1490d421387357af6ddc9e39e8a81e406731
Konstantin Cherkashin <che.constantine@gmail.com>
2016-03-22 13:33:53 -0300

    factor elements

--- src-cljs/misabogados_workflow/elements.cljs
+++ src-cljs/misabogados_workflow/elements.cljs
@@ -9,19 +9,16 @@
 (defn prepare-input [cursor form]
   ((juxt gen-name #(r/cursor form (into [] %))) cursor))
 
-(defn input [form type label cursor]
-  (let [[name cursor] (prepare-input cursor form)]
-    [:div.form-group {:key name}
-     [:label.control-label {:for name} label]
-     [:input.form-control {:type type
-                           :id name
-                           :value @cursor
-                           :on-change #(reset! cursor (-> % .-target .-value))
-                           }]]))
-
-(defn form-input [f & args]
-  (fn [[form]] (apply f form args)))
-
+(defn input [type label cursor]
+  (fn [[form]]
+    (let [[name cursor] (prepare-input cursor form)]
+      [:div.form-group {:key name}
+       [:label.control-label {:for name} label]
+       [:input.form-control {:type type
+                             :id name
+                             :value @cursor
+                             :on-change #(reset! cursor (-> % .-target .-value))
+                             }]])))
 
 (defn input-checkbox [label cursor]
   (fn [[form]]
@@ -41,8 +38,7 @@
        [:label.control-label {:for name} label]
        (into [:select.form-control {:id name
                                     :value @cursor
-                                    :on-change #(reset! cursor (-> % .-target .-value))
-                                    }]
+                                    :on-change #(reset! cursor (-> % .-target .-value))}]
              (map (fn [[label value]] [:option {:key value :value value} label]) options))])))
 
 (defn input-typeahead [label cursor]
@@ -51,43 +47,39 @@
           text (r/cursor options (into [:typeahead-t] cursor))
           dropdown-class (r/cursor options (into [:typeahead-c] cursor))
           options (get-in @options cursor)
-          [name cursor] (prepare-input cursor form)]
-      (if (nil? @text) (reset! text (some (fn [[l v]] (when (= v @cursor) l)) options)))
+          [name cursor] (prepare-input cursor form)
+          match #(some (fn [[l v]] (when (= v %) l)) options)]
+      (if (nil? @text) (reset! text (match @cursor)))
       [:div.form-group {:key name}
        [:label.control-label {:for name} label]
-       [:input.form-control {:type :text
-                             :value @text
-                             :on-click #(.setSelectionRange (.-target %) 0 (count @text))
-                             :on-focus #(js/setTimeout (fn [_] (reset! dropdown-class "open")) 100)
-                             :on-blur #(js/setTimeout (fn [_] (do (reset! dropdown-class "")
-                                                                 (reset! text (some (fn [[l v]]
-                                                                                      (when (= v @cursor) l)) options)))) 100)
-                             :on-change #(do (reset! text (-> % .-target .-value))
-                                             (reset! f-opts (filter (fn [[l]]
-                                                                      (re-find (re-pattern @text) l))
-                                                                    options)))}]
+       [:input.form-control
+        {:type :text
+         :value @text
+         :on-click #(.setSelectionRange (.-target %) 0 (count @text))
+         :on-focus #(js/setTimeout (fn [_] (reset! dropdown-class "open")) 100)
+         :on-blur #(js/setTimeout (fn [_] (do (reset! dropdown-class "")
+                                             (reset! text (match @cursor)))) 100)
+         :on-change #(do (reset! text (-> % .-target .-value))
+                         (reset! f-opts (filter
+                                         (fn [[l]] (re-find (re-pattern (s/lower-case @text)) (s/lower-case l)))
+                                         options)))}]
        [:div.dropdown {:class @dropdown-class}
         (into [:ul.dropdown-menu
                {:id name
-                :style {:height :auto :max-height :200px :overflow-x :hidden}
-                :value @cursor}]
-              (map (fn [[label value]]
-                     [:li {:key value
-                           :value value
-                           :label label
-                           :on-click #(do (reset! cursor (-> % .-target .-value))
-                                          (reset! text label))}
-                      label])
+                :style {:height :auto :max-height :200px :overflow-x :hidden}}]
+              (map (fn [[label value]] [:li {:key value
+                                            :on-click #(do (reset! cursor value)
+                                                           (reset! text label))} label])
                    (if @f-opts @f-opts options)))]])))
 
 
-(def input-text (partial form-input input :text))
+(def input-text (partial input :text))
 
-(def input-password (partial form-input input :password))
+(def input-password (partial input :password))
 
-(def input-email (partial form-input input :email))
+(def input-email (partial input :email))
 
-(def input-number (partial form-input input :number))
+(def input-number (partial input :number))
 
 (defn form [legend form-data & fieldsets]
   [:div.form-horizontal
