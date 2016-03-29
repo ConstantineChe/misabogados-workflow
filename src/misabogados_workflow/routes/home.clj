27e4ac900914b11cc6a37befb4557d83c474394e
Eugenio Panduro <panduro@misabogados.com>
2016-03-29 16:41:27 -0300

    public contact form

--- src/misabogados_workflow/routes/home.clj
+++ src/misabogados_workflow/routes/home.clj
@@ -9,7 +9,7 @@
             [ring.util.response :refer [redirect response]]
             [camel-snake-kebab.core :refer :all]
             [camel-snake-kebab.extras :refer [transform-keys]]
-            [misabogados-workflow.db.core :as db]
+            [misabogados-workflow.db.core :as db :refer [oid]]
             [monger.collection :as mc]
             [clojure.pprint :refer [pprint]]
             [misabogados-workflow.flow :refer [get-rendered-form dataset PManual PAutomatic]]
@@ -52,6 +52,10 @@
   (db/create-lead (assoc  (:lead (transform-keys ->snake_case_keyword params)) :step :check))
   (redirect "/#dashboard"))
 
+(defn create-lead-from-contact [{:keys [params]}]
+  (mc/insert @db/db "leads" (select-keys params [:client_name :client_phone :client_email :lead_type_code :problem]))
+  (redirect "/"))
+
 (defn edit-lead [id]
   (let [lead (db/get-lead id)]
     (layout/blank-page "Form"
@@ -80,6 +84,8 @@
 (defroutes home-routes
   (GET "/" [] home-page)
   (GET "/docs" [] (ok (-> "docs/docs.md" io/resource slurp)))
+  (GET "/contact" [] (render "contact.html"))
+  (POST "/contact" [] create-lead-from-contact)
   (GET "/lead/:id/edit" {{id :id} :params} (edit-lead id))
   (GET "/leads/create" [] new-lead)
   (PUT "/lead/:id" [id :as request] (update-lead id request))
--- src/misabogados_workflow/routes/payments.clj
+++ src/misabogados_workflow/routes/payments.clj
@@ -15,101 +15,7 @@
             [misabogados-workflow.access-control :as ac]
             [misabogados-workflow.middleware :as mw]
             [misabogados-workflow.util :as util]
-            [misabogados-workflow.email :as email]
-            [buddy.auth.accessrules :refer [restrict]]))
-
-(defn get-current-user [request]
-  (dbcore/get-user (:identity request)))
-
-(defn get-current-user-id [request]
-  (:_id (get-current-user request)))
-
-(defn access-error-handler [request value]
-  {:status 403
-   :header {}
-   :body {:error (str "not autherized, " value)
-          :role (-> request :session :role)}})
-
-(defn allowed-to-edit [id request]
-  (if (or (= :admin (-> request :session :role))
-          (= :finance (-> request :session :role))
-          (= (:lawyer (mc/find-one-as-map @db "payment_requests" {:_id id}))
-             (get-current-user-id request)))
-    true
-    {:message "Not allowed"}))
-
-(defn get-payment-requests [request]
-  (let [payment-requests (apply merge (map (fn [payment-request]
-                          {(str (:_id payment-request)) (dissoc payment-request :_id)})
-                                           (cond (= :lawyer (-> request :session :role))
-                                                 (dbcore/get-payment-requests (get-current-user-id request))
-                                                 (or (= :admin (-> request :session :role))
-                                                     (= :finance (-> request :session :role)))
-                                                 (map #(update-in % [:lawyer_data 0]
-                                                                  (fn [c] (into {} (filter
-                                                                                     (fn [f] (not (contains? #{:_id :password :verification-code}
-                                                                                                            (key f))))
-                                                                                     c))))
-                                                      (mc/aggregate @db "payment_requests"
-                                                                        [{"$lookup" {:from "users"
-                                                                                     :localField :lawyer
-                                                                                     :foreignField :_id
-                                                                                     :as :lawyer_data}}
-                                                                         ])))))]
-    (response {:payment-requests payment-requests :status "ok" :role (-> request :session :role)})))
-
-
-
-(defn get-payment-request [id request]
-  (response {:payment-request (mc/find-one-as-map @db "payment-requests" {:_id (oid id)})
-             :status "ok" :role (-> request :session :role)}))
-
-(defn create-payment-request [request]
-  (let [params (:params request)
-        current-user (get-current-user request)
-        payment-request (assoc (:params request)
-                                          :lawyer (:_id current-user)
-                                          :code (util/generate-hash params)
-                                          :date_created (new java.util.Date))]
-    (dbcore/create-payment-request payment-request)
-    (future (email/payment-request-email (:client_email params) {:lawyer-name (:name current-user)
-                                                                 :payment-request payment-request
-                                                                 :base-url (get (:headers request) "host")}))
-    (response {:payment-request {:create "new"}
-               :status "ok"
-               :role (-> request :session :role)
-               :params params})))
-
-(defn update-payment-request [id request]
-  (let [id (oid id)
-        params (:params request)
-        allowed? (allowed-to-edit id request)]
-    (if (true? allowed?)
-      (do (mc/update-by-id @db "payment_requests" id {$set
-                                                         (assoc (dissoc params :lawyer)
-                                                                :date_updated (new java.util.Date)
-                                                                :code (util/generate-hash (:params request)))})
-          (let [current-user (get-current-user request)
-                payment-request (mc/find-one-as-map @db "payment_requests" {:_id id})]
-            (println (str "-----" payment-request))
-            (future (email/payment-request-email (:client_email payment-request) {:lawyer-name (:name current-user)
-                                                                                  :payment-request payment-request
-                                                                                  :base-url (get (:headers request) "host")})))
-          (response {:payment-request {:update id} :status "ok" :role (-> request :session :role)}))
-      {:status 403
-       :header {}
-       :body {:error (:message allowed?)}})))
-
-(defn remove-payment-request [id request]
-  (let [id (oid id)
-        params (:params request)
-        allowed? (allowed-to-edit id request)]
-    (if (true? allowed?)
-      (do (mc/remove-by-id @db "payment_requests" id)
-          (response {:payment-request {:delete id} :status "ok" :role (-> request :session :role)}))
-      {:status 403
-       :header {}
-       :body {:error (:message allowed?)}})))
+            [misabogados-workflow.email :as email]))
 
 (def test-payment-data {
                         :merchantId "500238"
@@ -121,8 +27,8 @@
                         :tax "0"
                         :taxReturnBase "0"
                         :currency "MXN"
-                        ;; :signature "be2f083cb3391c84fdf5fd6176801278"
-                        :test "1"
+                        ;; :signature "be2f083cb3391c84fdf5fd6176801278" 
+                       :test "1"
                         :confirmationUrl "http://localhost:3000/payments/confirmation"
                         ;; :buyerEmail "test@test.com"
                         })
