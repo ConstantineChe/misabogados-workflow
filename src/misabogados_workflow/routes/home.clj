4035c2e0218cf222b520ef937fa105081c15dc30
Konstantin Cherkashin <che.constantine@gmail.com>
2016-04-25 14:10:56 -0300

    sente

--- src/misabogados_workflow/routes/home.clj
+++ src/misabogados_workflow/routes/home.clj
@@ -14,6 +14,7 @@
             ))
 
 (defn home-page [request]
+  (clojure.pprint/pprint request)
   (render "home_page.html" {:title "Recomendamos Abogados Confiables"
                             :logged-in? (authenticated? request)
                             :user (db/get-user (:identity request))}))
