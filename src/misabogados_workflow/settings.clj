be37693e005c85f3e1d01f090626b357a6e49186
Eugenio Panduro <panduro@misabogados.com>
2016-05-04 15:12:47 -0300

    settings are being saved now

--- src/misabogados_workflow/settings.clj
+++ src/misabogados_workflow/settings.clj
@@ -1,11 +1,15 @@
 (ns misabogados-workflow.settings
   (:require [monger.core :as mg]
+            [monger.operators :refer :all]
             [monger.collection :as mc]
             [config.core :refer [env]]))
 
 (defonce settings (atom {}))
 
 (defonce db (atom nil))
+
+(defn get-country []
+  (or (:country env) "cl"))
 
 (defn connect! []
   (reset! db (-> (:settings-database-url env)
@@ -17,9 +21,14 @@
     (reset! db nil)))
 
 (defn reload! [] 
-  (let [data (mc/find-one-as-map @db "settings" {:country (or (:country env) "cl")})] 
-    (println (str "~~SETTINGS RECEIVED " data)) 
+  (let [data (mc/find-one-as-map @db "settings" {:country (get-country)})] 
     (reset! settings data)))
+
+(defn save! [data] 
+  (mc/update @db "settings" 
+             {:country (get-country)} 
+             {$set (dissoc data :country)})
+  (reload!))
 
 (defn init! [] (do (connect!)
                    (reload!)))
