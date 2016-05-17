161d9abf5b153c5330480da31725ec728ab7e3ec
Eugenio Panduro <panduro@misabogados.com>
2016-05-17 17:44:29 -0400

    experimental confirmation page

--- src/misabogados_workflow/routes/payments.clj
+++ src/misabogados_workflow/routes/payments.clj
@@ -100,6 +100,9 @@
 
 (defmulti get-payment-request-by-payment-code (fn [request] (settings/fetch :payment_system)))
 
+(defmethod get-payment-request-by-payment-code "payu" [request]
+  (mc/find-one-as-map @db "payment_requests" {:payment_log {$elemMatch {"data.referenceCode" (-> request :params :referenceCode)}}}))
+
 (defmethod get-payment-request-by-payment-code "webpay" [request]
   (mc/find-one-as-map @db "payment_requests" {:payment_log {$elemMatch {"data.TBK_ORDEN_COMPRA" (-> request :params :TBK_ORDEN_COMPRA)}}}))
 
